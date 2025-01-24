5f8805cc6ee7b1136a1d35bb4a60fef301a8680e
Edith Gómez <edith.gomez4@outlook.com>
2025-01-24 13:50:21 -0600

    Terminada página Inicio

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,19 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    const spanElement = document.querySelector('.num');
+    const targetNumber = parseInt(spanElement.getAttribute('data-to'));
+    const duration = parseInt(spanElement.getAttribute('data-time'));
+    const increment = 1;
+
+    let currentNumber = 0;
+    const interval = setInterval(() => {
+        currentNumber += increment;
+        if (currentNumber >= targetNumber) {
+            currentNumber = targetNumber;
+            clearInterval(interval);
+        }
+        spanElement.textContent = Math.floor(currentNumber);
+    }, 200);
+});
