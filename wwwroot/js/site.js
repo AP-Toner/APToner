7610efcf6bf3ce0106e95821eeb6f47d8310bb65
Edith Gómez <edith.gomez4@outlook.com>
2025-01-28 13:48:01 -0600

    Estructura base Tienda

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,17 +4,31 @@
 /* "Animación" áumento de números */
 document.addEventListener("DOMContentLoaded", function () {
     const spanEl = document.getElementById("clientes");
-    const numFinal = parseInt(spanEl.getAttribute('data-to'));
-    const duracion = parseInt(spanEl.getAttribute('data-time'));
-    const incremento = 1;
+    const prodTitulo = document.querySelectorAll('.producto-title');
 
-    let numActual = 0;
-    const intervalo = setInterval(() => {
-        numActual += incremento;
-        if (numActual >= numFinal) {
-            numActual = numFinal;
-            clearInterval(intervalo);
-        }
-        spanEl.textContent = Math.floor(numActual);
-    }, duracion);
+    if (spanEl) {
+        const numFinal = parseInt(spanEl.getAttribute('data-to'));
+        const duracion = parseInt(spanEl.getAttribute('data-time'));
+        const incremento = 1;
+
+        let numActual = 0;
+        const intervalo = setInterval(() => {
+            numActual += incremento;
+            if (numActual >= numFinal) {
+                numActual = numFinal;
+                clearInterval(intervalo);
+            }
+            spanEl.textContent = Math.floor(numActual);
+        }, duracion);
+    }
+
+    if (prodTitulo) {
+        prodTitulo.forEach(title => {
+            let text = title.textContent.toLowerCase();
+
+            //text = text.replace(/\b\w/g, char => char.toUpperCase());
+
+            title.textContent = text;
+        });
+    }
 });
